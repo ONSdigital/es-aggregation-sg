261d7aba702202ee191be222d1c7a8204e6b4a70
Jordancooke <9076311+Jordancooke@users.noreply.github.com>
2019-09-10 16:23:44 +0100

    Adding doc string to the lambda_handler in the method

--- aggregation_entref_method.py
+++ aggregation_entref_method.py
@@ -7,6 +7,14 @@ lambda_client = boto3.client('lambda', region_name='eu-west-2')
 
 
 def lambda_handler(event, context):
+    """
+    This method is responsible for grouping the data by county, region
+    and period. It then aggregates on enterprise_reference creating a
+    count and then renames the column accordingly.
+    :param event: The data you wish to perform this method on.
+    :param context:
+    :return: output_json: The grouped and aggregated data. - Type: JSON
+    """
     current_module = "Aggregation CalculateEnterpriseRef - Method"
     logger = logging.getLogger("Entref")
     error_message = ""
