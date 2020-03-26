62ec744e13d5f48f79dddf2913109448a170a56f
Michael Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-03-26 15:35:26 +0000

    Update aggregation_bricks_splitter_wrangler.py

--- aggregation_bricks_splitter_wrangler.py
+++ aggregation_bricks_splitter_wrangler.py
@@ -111,7 +111,8 @@ def lambda_handler(event, context):
         payload = {
             "json_data": json.loads(data_region),
             "regionless_code": regionless_code,
-            "region_column": region_column
+            "region_column": region_column,
+            "RuntimeVariables": {"run_id": run_id}
         }
 
         # Pass the data for processing (adding of the regionless region.
